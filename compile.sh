134d9eca6ee7d9743d5b89ab84ccb575daafa2e8
Rob McKay <robert.mckay@mining.komatsu>
2022-01-21 16:21:17 +0000

    Updated build script

--- compile.sh
+++ compile.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 export CROSS_COMPILE="arm-none-eabi-"
 
+rm -rf build-RS20n
+mkdir build-RS20n
+
 # remove Remove all generated files + config + various backup files
-# make clean || exit $?
+# make distclean || exit $?
+make O=build-RS20n distclean || exit $?
 
-make sama5d3_xplained_nandflash_defconfig || exit $?
+#make sama5d3_xplained_nandflash_defconfig || exit $?
+make O=build-RS20n RS20n-sama5d3_nandflash_defconfig || exit $?
 
-make
+make O=build-RS20n  all || exit $?
+
+#mv u-boot.bin u-boot-RS20n.bin
+#mv u-boot.map u-boot-RS20n.map
+
